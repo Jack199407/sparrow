b35af3b17f778d7162e49724f9b25d6ec90b0463
Jack <zhaoke199407@gmail.com>
2024-05-07 13:52:57 -0400

    sparrow-adapter

---
+++ sparrow-adapter/src/main/java/com/algonquin/adapter/SparrowAdapterApplication.java
@@ -0,0 +1,13 @@
+package com.algonquin.adapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparrowAdapterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SparrowAdapterApplication.class, args);
+    }
+
+}
